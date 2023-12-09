dd2b9daf21245ad62b15ae913a2104771a1e8b40
Shannon2023Fall <shanshansepost@outlook.com>
2023-12-09 21:47:54 +0100

    Buttons of Edit and Delete are deepened with fucntions but still

--- src/CardsEditor.jsx
+++ src/CardsEditor.jsx
@@ -9,10 +9,22 @@ export default function Multifunction() {
 // Project tasks of week 8
     const handleEdit = (id) => {
       console.log("Edit", id);
+      // Find the index of the card to be edited due to batch placed buttons
+      const index = acciData.findIndex(acci => acci.id === id);
+      // Create a copy of the acciData array
+      const updatedAcciData = [...acciData];
+      // Set the 'editable' property of the card to true
+      updatedAcciData[index].editable = true;
+      // Update the state with the updatedAcciData
+      setAcciData(updatedAcciData);
     };
 
     const handleDelete = (id) => {
       console.log("Delete", id);
+      // Filter out the card with the given id
+      const updatedAcciData = acciData.filter(acci => acci.id === id);
+      // Update the state with the updatedAcciData
+      setAcciData(updatedAcciData);
     };
 
     return (
@@ -28,9 +40,21 @@ export default function Multifunction() {
                             <h6>ID {acci.id}</h6>
                         </div>
                         <ul class="list-group list-group-flush">
-                            <li class="list-group-item" >{acci.gender}</li>
-                            <li class="list-group-item" >Age {acci.age}</li>
-                            <li class="list-group-item" >{acci.month}</li>
+                            {acci.editable ? (
+                                <>
+                                    <li class="list-group-item" ><input type="text" value={acci.gender} onChange={(e) => acci.gender = e.target.value} /></li>
+                                    <li class="list-group-item" >Age <input type="number" value={acci.age} onChange={(e) => acci.age = e.target.value} /></li>
+                                    <li class="list-group-item" ><input type="text" value={acci.month} onChange={(e) => acci.month = e.target.value} /></li>
+                                
+                                
+                                </>
+                            ) : (
+                                <>
+                                <li className="list-group-item">{acci.gender}</li>
+                                <li className="list-group-item">Age {acci.age}</li>
+                                <li className="list-group-item">{acci.month}</li>
+                                </>
+                            )}                            
                         </ul>
                         <div>
                             <button type="button" class="btn btn-outline-dark  btn-sm me-2 mt-3" onClick={()=>handleEdit(acci.id)}>Edit</button>
